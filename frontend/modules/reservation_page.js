77ca2eb9f1a9498ab1d3158e8bdac82af6b96d66
CrioUser <crio-user@criodo.com>
2022-07-19 00:30:16 +0530

    ME_QTRIPDYNAMIC_MODULE_RESERVATIONS

--- frontend/modules/reservation_page.js
+++ frontend/modules/reservation_page.js
@@ -20,20 +20,27 @@ async function fetchReservations() {
 function addReservationToTable(reservations) {
   // TODO: MODULE_RESERVATIONS
   // 1. Add the Reservations to the HTML DOM so that they show up in the table
-  let reservationTableParent = document.getElementById(
-    "reservation-table-parent"
-  );
-  let noReservationBanner = document.getElementById("no-reservation-banner");
 
-  //Conditionally render the no-reservation-banner and reservation-table-parent
-  if (reservations.length >= 0) {
-    reservationTableParent.style.display = "block";
-    noReservationBanner.style.display = "none";
+
+  /*
+    Iterating over reservations, adding it to table (into div with class "reservation-table") and link it correctly to respective adventure
+    The last column of the table should have a "Visit Adventure" button with id=<reservation-id>, class=reservation-visit-button and should link to respective adventure page
+
+    Note:
+    1. The date of adventure booking should appear in the format D/MM/YYYY (en-IN format) Example:  4/11/2020 denotes 4th November, 2020
+    2. The booking time should appear in a format like 4 November 2020, 9:32:31 pm
+  */
+  if (reservations.length > 0) {
+
+    document.getElementById("no-reservation-banner").style.display = "none";
+    document.getElementById("reservation-table-parent").style.display = "block";
 
     reservations.map((reservation) => {
       let tRow = document.createElement("tr");
-      let convertedDate = new Date(reservation.time).toLocaleDateString("en-IN");
-      let convertedTime = new Date(reservation.date).toLocaleString("en-IN", {
+      let convertedDate = new Date(reservation.date).toLocaleDateString(
+        "en-IN"
+      );
+      let convertedTime = new Date(reservation.time).toLocaleString("en-IN", {
         year: "numeric",
         day: "numeric",
         month: "long",
@@ -42,39 +49,30 @@ function addReservationToTable(reservations) {
         second: "numeric",
         hour12: true,
       });
-      tRow.setAttribute("id", `${reservation.id}`);
       tRow.innerHTML = `
-        <a href = "../detail/?adventure=${reservation.adventure}"<td>${reservation.id}</a>
-        </td>
-        <td>${reservation.name}</td>
-        <td>${reservation.adventureName}</td>
-        <td>${reservation.person}</td>
-        <td>${convertedDate}</td>
-        <td>${reservation.price}</td>
-        <td>${convertedTime}</td>
-        <td>
-         <div class="reservation-visit-button" id=${reservation.id}>
-         <a href = "../detail/?adventure=${reservation.adventure}">Visit Adventure</button></a>
-        </td>
+      <td><b>${reservation.id}</b></td>
+      <td>${reservation.name}</td>
+      <td>${reservation.adventureName}</td>
+      <td>${reservation.person}</td>
+      <td>${convertedDate}</td>
+      <td>${reservation.price}</td>
+      <td>${convertedTime}</td>
+      <td>
+      <div class="reservation-visit-button" id=${reservation.id}>
+          <a href="../detail/?adventure=${reservation.adventure}">Visit Adventure</a>
+      </div>
+      </td>
     `;
-      document.getElementById("reservation-table").append(tRow);
+      document.getElementById("reservation-table").appendChild(tRow);
     });
+
   } else {
-    reservationTableParent.style.display = "none";
-    noReservationBanner.style.display = "block";
+    document.getElementById("no-reservation-banner").style.display = "block";
+    document.getElementById("reservation-table-parent").style.display = "none";
   }
 
 }
 
 
-
-/*
-    Iterating over reservations, adding it to table (into div with class "reservation-table") and link it correctly to respective adventure
-    The last column of the table should have a "Visit Adventure" button with id=<reservation-id>, class=reservation-visit-button and should link to respective adventure page
-
-    Note:
-    1. The date of adventure booking should appear in the format D/MM/YYYY (en-IN format) Example:  4/11/2020 denotes 4th November, 2020
-    2. The booking time should appear in a format like 4 November 2020, 9:32:31 pm
-  */
 
 export { fetchReservations, addReservationToTable };
